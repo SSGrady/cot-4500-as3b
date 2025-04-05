39f118604884e7365494dffe50c9995d5899ae4b
Steven Grady <stevenagrady@gmail.com>
2025-04-04 22:24:04 -0400

    chore(init): Setup main/ and test/

---
+++ src/main/assignment_3.py
@@ -0,0 +1,11 @@
+def gaussian_elimination(mat, vec):
+    pass
+
+def lu_factorization(mat):
+    pass
+
+def diagonally_dominant(mat):
+    pass
+
+def positive_definite(mat):
+    pass
---
+++ src/test/test_assignment_3.py
@@ -0,0 +1,10 @@
+class Testing:
+    def __init__(self):
+        pass
+
+    def run_tests(self):
+        pass
+
+if __name__ == "__main__":
+    tester = Testing()
+    tester.run_tests()
